65e511ffc37532838378baa53cca634d8a2d8315
Gabriel Gomez <gago.852@outlook.com>
2020-02-24 23:58:01 -0500

    creado interfaz grafica. integracion inicial con login de facebook
    Signed-off-by: Gabriel Gomez <gago.852@outlook.com>

--- app/src/main/java/com/gago/login_facebook/MainActivity.java
+++ app/src/main/java/com/gago/login_facebook/MainActivity.java
@@ -1,18 +1,20 @@
 package com.gago.login_facebook;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button registrarse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        registrarse= findViewById(R.id.btRegistrarse);
+
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent i=new Intent(getApplicationContext(),RegistroActivity.class);
+            startActivity(i);
+            }
+        });
 
 
     }
