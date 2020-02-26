df65a7656f5c318e2bf9f47b8f1269a7f19a41b0
Gabriel Gomez <gago.852@outlook.com>
2020-02-25 23:03:05 -0500

    implementado login con facebook
    Signed-off-by: Gabriel Gomez <gago.852@outlook.com>

--- app/src/main/java/com/gago/login_facebook/MainActivity.java
+++ app/src/main/java/com/gago/login_facebook/MainActivity.java
@@ -3,6 +3,7 @@ package com.gago.login_facebook;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,6 +15,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     TextInputEditText edUsuario, edPass;
     Button btRegistrarse, btlogueo;
+    LoginButton loginButton;
 
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +43,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        callbackManager = CallbackManager.Factory.create();
 
         edUsuario = findViewById(R.id.edUsuario);
         edPass = findViewById(R.id.edPass);
         btlogueo = findViewById(R.id.btLogin);
         btRegistrarse = findViewById(R.id.btRegistrarse);
+        loginButton = findViewById(R.id.login_button);
 
         Intent i = getIntent();
         Log.i("intent", i.toString());
-        Bundle retorno = i.getExtras() == null ? new Bundle() : i.getExtras() ;
+        Bundle retorno = i.getExtras() == null ? new Bundle() : i.getExtras();
         user = retorno.getString("usuario", "");
         nombre = retorno.getString("nombre", "");
         pass = retorno.getString("password", "");
@@ -51,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 if (!TextUtils.isEmpty(user) && !TextUtils.isEmpty(pass)) {
                     if (!TextUtils.isEmpty(edUsuario.getText()) && !TextUtils.isEmpty(edPass.getText())) {
                         if (user.contentEquals(edUsuario.getText()) && pass.contentEquals(edPass.getText())) {
-                            Toast.makeText(getApplicationContext(), "usuario correcto bienvenido señor "+nombre, Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "usuario correcto bienvenido señor " + nombre, Toast.LENGTH_LONG).show();
                             Log.i("usuarioCorrecto", "correcto");
                         } else {
                             Toast.makeText(getApplicationContext(), "usuario incorrecto", Toast.LENGTH_LONG).show();
@@ -68,6 +82,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        loginButton.setPermissions("email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken=AccessToken.getCurrentAccessToken();
+                boolean isLogFace=accessToken != null && !accessToken.isExpired();
+                if (isLogFace){
+                    Toast.makeText(getApplicationContext(),"logueado",Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
         btRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,8 +113,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        /*if (isLogFace){
+            Bundle params=new Bundle();
+            params.putString("name","email");
+
+            GraphRequest.Callback callback=new GetUs
+            GraphRequest request=new GraphRequest(accessToken,"/me",params, HttpMethod.GET,)
+        }*/
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
